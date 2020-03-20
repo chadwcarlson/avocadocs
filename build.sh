8ef810c943fa9d5e44a93bb5795788976e166ed0
chadcarlson <chad.carlson@platform.sh>
2020-03-20 14:20:15 -0400

    Change build command.

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 HUGOVERSION=0.66.0
 
-cd exampleSite
-
 wget https://github.com/gohugoio/hugo/releases/download/v$HUGOVERSION/hugo_${HUGOVERSION}_Linux-64bit.tar.gz
 tar xvzf hugo_${HUGOVERSION}_Linux-64bit.tar.gz
 
@@ -9,9 +7,7 @@ rm hugo_${HUGOVERSION}_Linux-64bit.tar.gz
 
 # npm run prod
 
-pwd
-cd exampleSite
 # ./hugo
 # pwd
 # HUGO_THEME=avocadocs ./hugo --themesDir ../../
-./hugo --themesDir ../../
+./hugo -s exampleSite
