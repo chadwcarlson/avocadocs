dd17ca9ea31fc559d7b31c87250bc0573a4f3d1a
chadcarlson <chad.carlson@platform.sh>
2020-03-20 15:00:03 -0400

    Copy magic.

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 HUGOVERSION=0.66.0
 
+cd exampleSite
+
+mkdir exampleSite/themes
+mkdir exampleSite/themes/avocadocs
+
+cp -r !(exampleSite) exampleSite/themes/avocadocs
+
 wget https://github.com/gohugoio/hugo/releases/download/v$HUGOVERSION/hugo_${HUGOVERSION}_Linux-64bit.tar.gz
 tar xvzf hugo_${HUGOVERSION}_Linux-64bit.tar.gz
 
@@ -10,4 +17,4 @@ rm hugo_${HUGOVERSION}_Linux-64bit.tar.gz
 # ./hugo
 # pwd
 # HUGO_THEME=avocadocs ./hugo --themesDir ../../
-./hugo -s exampleSite --themesDir ../../ --theme avocadocs --debug
+# ./hugo -s exampleSite --themesDir ../../ --theme avocadocs --debug
