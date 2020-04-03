9484ba458127b1fe340c8cfe01ae7ea52b2d5081
Jonas Kroeger <jonas.kroeger@platform.sh>
2020-04-03 10:36:58 +0200

    cleanup

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -1,60 +1,57 @@
-const gulp = require("gulp");
-const postcss = require("gulp-postcss");
-const cssnano = require("cssnano");
-const browserify = require("browserify");
-const babelify = require("babelify");
-const source = require("vinyl-source-stream");
-const buffer = require("vinyl-buffer");
-const uglify = require("gulp-uglify");
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
 
 
 function cssBuild() {
     return gulp
-        .src(`./public/docsuikit/assets/css/*.css`)
-        .pipe(postcss([cssnano()]))
-        .pipe(gulp.dest(`./public/docsuikit/assets/css/`));
+      .src('./public/docsuikit/assets/css/*.css')
+      .pipe(postcss([cssnano()]))
+      .pipe(gulp.dest('./public/docsuikit/assets/css/'));
 }
 
 function javascriptBuild() {
   return (
       browserify({
-          entries: [[
-            './public/docsuikit/assets/js/main.js',
-            './public/docsuikit/assets/js/autocomplete.js',
-            './public/docsuikit/assets/js/custom-scrollbar.js'
-            // './public/docsuikit/assets/js/sticky-sidebar.js',
-            // './public/docsuikit/assets/js/header-fixing.js',
-            // './public/docsuikit/assets/js/theme-custom.js'
-          ]],
-          transform: [babelify.configure({ presets: ["@babel/preset-env"] })]
+        entries: [[
+          './public/docsuikit/assets/js/main.js',
+          './public/docsuikit/assets/js/autocomplete.js',
+          './public/docsuikit/assets/js/custom-scrollbar.js'
+        ]],
+        transform: [babelify.configure({ presets: ['@babel/preset-env'] })]
       })
-          .bundle()
-          .pipe(source("bundle.js"))
-          // Turn it into a buffer!
-          .pipe(buffer())
-          // And uglify
-          .pipe(uglify())
-          .pipe(gulp.dest(`./public/docsuikit/assets/js/`))
+      .bundle()
+      .pipe(source('bundle.js'))
+      // Turn it into a buffer!
+      .pipe(buffer())
+      // And uglify
+      .pipe(uglify())
+      .pipe(gulp.dest('./public/docsuikit/assets/js/'))
   );
 }
 
 function javascriptBuildWidgets() {
   return (
       browserify({
-          entries: [[
-            './public/docsuikit/assets/vendor/jquery-ui/ui/widgets/autocomplete.js',
-            './public/docsuikit/assets/vendor/jquery-ui/ui/widgets/menu.js',
-            './public/docsuikit/assets/vendor/jquery-ui/ui/widgets/mouse.js',
-          ]],
-          transform: [babelify.configure({ presets: ["@babel/preset-env"] })]
+        entries: [[
+          './public/docsuikit/assets/vendor/jquery-ui/ui/widgets/autocomplete.js',
+          './public/docsuikit/assets/vendor/jquery-ui/ui/widgets/menu.js',
+          './public/docsuikit/assets/vendor/jquery-ui/ui/widgets/mouse.js',
+        ]],
+        transform: [babelify.configure({ presets: ['@babel/preset-env'] })]
       })
-          .bundle()
-          .pipe(source("bundle.widgets.js"))
-          // Turn it into a buffer!
-          .pipe(buffer())
-          // And uglify
-          .pipe(uglify())
-          .pipe(gulp.dest(`./public/docsuikit/assets/js/`))
+      .bundle()
+      .pipe(source('bundle.widgets.js'))
+      // Turn it into a buffer!
+      .pipe(buffer())
+      // And uglify
+      .pipe(uglify())
+      .pipe(gulp.dest('./public/docsuikit/assets/js/'))
   );
 }
 
@@ -63,7 +60,7 @@ function javascriptMinifyAsciiplayer() {
     .src('./public/scripts/asciinema-player.js')
     .pipe(buffer())
     .pipe(uglify())
-    .pipe(gulp.dest(`./public/scripts/`))
+    .pipe(gulp.dest('./public/scripts/'))
 }
 
 
